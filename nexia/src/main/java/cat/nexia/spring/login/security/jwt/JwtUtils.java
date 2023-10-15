9c82019c7d53b935f52ece8a3b5b336c5f7f83f0
cristian <cristianpinol@pinol.com>
2023-10-15 20:50:35 +0200

    Documentades les classes de seguretat

--- nexia/src/main/java/cat/nexia/spring/login/security/jwt/JwtUtils.java
+++ nexia/src/main/java/cat/nexia/spring/login/security/jwt/JwtUtils.java
@@ -15,6 +15,12 @@ import org.springframework.web.util.WebUtils;
 import cat.nexia.spring.login.security.services.UserDetailsImpl;
 import io.jsonwebtoken.*;
 
+/**
+ * Utility class for managing JWT tokens in security application.
+ * Allows you to generate, validate and obtain JWT token information, as well as
+ * manage
+ * JWT related cookies.
+ */
 @Component
 public class JwtUtils {
   private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
@@ -28,6 +34,12 @@ public class JwtUtils {
   @Value("${nexia.app.jwtCookieName}")
   private String jwtCookie;
 
+  /**
+   * Gets the JWT token from the cookies of an HTTP request.
+   *
+   * @param request The HTTP request.
+   * @return The JWT token as a string, or null if not found.
+   */
   public String getJwtFromCookies(HttpServletRequest request) {
     Cookie cookie = WebUtils.getCookie(request, jwtCookie);
     if (cookie != null) {
@@ -37,21 +49,45 @@ public class JwtUtils {
     }
   }
 
+  /**
+   * Generates a JWT cookie from a UserDetailsImpl object.
+   *
+   * @param userPrincipal The UserDetailsImpl of the authenticated user.
+   * @return A ResponseCookie containing the JWT token.
+   */
   public ResponseCookie generateJwtCookie(UserDetailsImpl userPrincipal) {
     String jwt = generateTokenFromUsername(userPrincipal.getUsername());
-    ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true).build();
+    ResponseCookie cookie = ResponseCookie.from(jwtCookie, jwt).path("/api").maxAge(24 * 60 * 60).httpOnly(true)
+        .build();
     return cookie;
   }
 
+  /**
+   * Gets an empty JWT cookie, used to delete the JWT cookie.
+   *
+   * @return A ResponseCookie with a null value.
+   */
   public ResponseCookie getCleanJwtCookie() {
     ResponseCookie cookie = ResponseCookie.from(jwtCookie, null).path("/api").build();
     return cookie;
   }
 
+  /**
+   * Gets the username from a JWT token.
+   *
+   * @param token The JWT token to parse.
+   * @return The username contained in the token.
+   */
   public String getUserNameFromJwtToken(String token) {
     return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
   }
 
+  /**
+   * Validates a JWT token.
+   *
+   * @param authToken The JWT token to validate.
+   * @return true if the token is valid, false if it is not.
+   */
   public boolean validateJwtToken(String authToken) {
     try {
       Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
@@ -70,8 +106,14 @@ public class JwtUtils {
 
     return false;
   }
-  
-  public String generateTokenFromUsername(String username) {   
+
+  /**
+   * Generates a JWT token from the given username.
+   *
+   * @param username The username for which the token is generated.
+   * @return The JWT token generated as a string.
+   */
+  public String generateTokenFromUsername(String username) {
     return Jwts.builder()
         .setSubject(username)
         .setIssuedAt(new Date())
