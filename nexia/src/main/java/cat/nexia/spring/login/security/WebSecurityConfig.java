d5206c810655c9aa7db5cb6f44de306afee61a8c
josep.faneca@ayesa.com <josep.faneca@ayesa.com>
2023-10-23 10:08:05 +0200

    JFT: Eliiminar config per a swagger

--- nexia/src/main/java/cat/nexia/spring/login/security/WebSecurityConfig.java
+++ nexia/src/main/java/cat/nexia/spring/login/security/WebSecurityConfig.java
@@ -23,9 +23,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(
-        // securedEnabled = true,
+         securedEnabled = true
         // jsr250Enabled = true,
-        prePostEnabled = true)
+        //prePostEnabled = true
+        // )
+)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     UserDetailsServiceImpl userDetailsService;
@@ -79,8 +81,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // disable page caching
         http.headers().cacheControl();
-
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+                "/configuration/security", "/swagger-ui.html", "/webjars/**", "/v2/swagger.json");
+    }
+
+
 
 
 }
